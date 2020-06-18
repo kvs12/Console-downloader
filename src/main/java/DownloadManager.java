1ee66b2ca96aa2dd7057c5b11bcb2ec2652215dc
kvs12 <kvs35@yandex.com>
2020-06-19 00:40:26 +0300

    check http return code; cleanup

--- src/main/java/DownloadManager.java
+++ src/main/java/DownloadManager.java
@@ -7,34 +7,41 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.protocol.HttpContext;
 
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by iddqd on 8/8/15.
  */
 public class DownloadManager {
-    private static PoolingHttpClientConnectionManager connectionManager;
     /* Download limit per thread in bytes */
-    private static long                               downloadLimit;
-    private        CloseableHttpClient                httpClient;
-    private        HashMap<String, String>            linksAndFilenames;
-    private        HttpGet                            httpGet;
-    private        String                             outputDest;
+    private static long downloadLimit;
+    private final CloseableHttpClient httpClient;
+    private final Map<String, String> linksAndFilenames;
+    private final String outputDest;
 
+    private static final AtomicLong bytesCounter = new AtomicLong();
+    private final List<GetDownloader> downloaders = new ArrayList<>();
+    private static final String ERROR_MESSAGE = "Can't start downloading: %s\n"
+            + "It might be related to internet connection";
 
-    private static       AtomicCounter       bytesCounter  = new AtomicCounter();
-    private              List<GetDownloader> downloaders   = new ArrayList<>();
-    private static final String              ERROR_MESSAGE = "Can't start downloading: %s\n" + "It might be related to internet connection";
-
-    DownloadManager(HashMap<String, String> linksAndFilenames, String outputDest, int poolSize, long speedLimit) throws IOException {
+    public DownloadManager(
+            Map<String, String> linksAndFilenames,
+            String outputDest,
+            int poolSize,
+            long speedLimit
+    ) {
         downloadLimit = speedLimit / poolSize;
-        connectionManager = new PoolingHttpClientConnectionManager();
+        PoolingHttpClientConnectionManager connectionManager =
+                new PoolingHttpClientConnectionManager();
         connectionManager.setMaxTotal(poolSize);
         /* In case it can be multiple downloads from the same host */
         connectionManager.setDefaultMaxPerRoute(poolSize);
@@ -46,7 +53,7 @@ public class DownloadManager {
 
     public void collectDownloaders() {
         for (Map.Entry<String, String> entry : linksAndFilenames.entrySet()) {
-            httpGet = new HttpGet(entry.getValue());
+            HttpGet httpGet = new HttpGet(entry.getValue());
             String filename = entry.getKey();
             Path outputPath = Paths.get(this.outputDest, filename);
             downloaders.add(new GetDownloader(httpClient, httpGet, outputPath));
@@ -66,14 +73,14 @@ public class DownloadManager {
     }
 
     public long getBytesCounter() {
-        return bytesCounter.getValue();
+        return bytesCounter.get();
     }
 
-    static class GetDownloader extends Thread {
+    private static class GetDownloader extends Thread {
         private final CloseableHttpClient httpClient;
-        private final HttpContext         context;
-        private final HttpGet             httpGet;
-        private final Path                destination;
+        private final HttpContext context;
+        private final HttpGet httpGet;
+        private final Path destination;
 
         GetDownloader(CloseableHttpClient httpClient, HttpGet httpGet, Path destination) {
             this.httpClient = httpClient;
@@ -85,26 +92,25 @@ public class DownloadManager {
 
         private void writeToFile(InputStream inputStream, String outputDest, long contentLength) throws IOException {
             try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(outputDest))) {
-
                 System.out.println("Starting to write " + outputDest);
 
                 int getBytes;
                 while ((getBytes = inputStream.read()) != -1) {
                     bufferedOutputStream.write(getBytes);
                 }
-
-                bytesCounter.increment(contentLength);
-
-            } catch (IOException e) {
-                throw e;
+                bytesCounter.addAndGet(contentLength);
             }
-
         }
 
         @Override
         public void run() {
             try (CloseableHttpResponse response = httpClient.execute(httpGet, context)) {
-
+                int code = response.getStatusLine().getStatusCode();
+                if (code != 200) {
+                    System.out.printf("Expected response code 200 while getting data, got %d\n" +
+                            "URL: %s\n", code, httpGet.getURI().toURL());
+                    return;
+                }
                 HttpEntity entity = response.getEntity();
                 InputStream inputStream;
                 if (downloadLimit != 0) {
@@ -114,7 +120,6 @@ public class DownloadManager {
                 }
 
                 writeToFile(inputStream, destination.toString(), entity.getContentLength());
-
             } catch (IOException e) {
                 System.out.println(String.format(ERROR_MESSAGE, e.getMessage()));
             }
